be8fd70b841878fffb2cb3ea1ad1e36b9ffe15a4
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-04-01 15:58:17 +0300

    walk animation

--- Yelloz/Assets/Scripts/TopDownMovement.cs
+++ Yelloz/Assets/Scripts/TopDownMovement.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 public class TopDownMovement : MonoBehaviour
 {
     private Vector3 playerVelocity;
-    [SerializeField] private int speed;
-    [SerializeField] private int rotateSpeed;
+    [SerializeField] private float speed;
+    [SerializeField] private float rotateSpeed;
     [SerializeField] private float gravity;
     [SerializeField] private bool isGrounded;
     [SerializeField] private float jumpHeight; 
     private CharacterController controller;
+    private Animator animator;
     
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
+    
     private void Update()
     {
         isGrounded = controller.isGrounded;
+        
         if (isGrounded && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
@@ -26,7 +30,6 @@ public class TopDownMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
-        //Vector3 moveDirection = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
         moveDirection.Normalize();
        
         controller.Move(moveDirection * (speed * Time.deltaTime));
@@ -35,6 +38,12 @@ public class TopDownMovement : MonoBehaviour
         {
             Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotateSpeed * Time.deltaTime);
+            
+            animator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
         }
 
         if (Input.GetButtonDown("Jump") && isGrounded)
